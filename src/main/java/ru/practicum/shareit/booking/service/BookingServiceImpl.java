cfffeeccd0b5fd81dc7855bd4f1f1321948ed30a
Illarionov Alex <linedaykek@gmail.com>
2023-06-20 08:23:35 +0300

    fix: using equals in appropriate way

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -22,7 +22,6 @@ import ru.practicum.shareit.user.repository.UserRepository;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -44,7 +43,7 @@ public class BookingServiceImpl implements BookingService {
     public BookingDto findById(Long id, Long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
         Booking booking = bookingRepository.findById(id).orElseThrow(() -> new NotFoundException("Booking not found"));
-        if (booking.getBooker().getId().equals(userId) && booking.getItem().getOwner().getId().equals(userId)) {
+        if (!booking.getBooker().getId().equals(userId) && !booking.getItem().getOwner().getId().equals(userId)) {
             throw new NotFoundException("Only booker or owner has an access to booking");
         }
         return BookingMapper.bookingToBookingDto(booking);
@@ -52,25 +51,31 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public List<BookingDto> findByState(String state, Long userId) {
-        List<Booking> bookings = new ArrayList<>();
+        List<Booking> bookings;
         User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
         Sort sort = Sort.by("end").descending();
         log.info(new Date().toString());
-        if (state.equals("ALL")) {
-            bookings = bookingRepository.findByBookerId(userId, sort);
-        } else if (state.equals("CURRENT")) {
-            bookings = bookingRepository.findByBookerIdAndEndIsAfterAndStartIsBefore(userId, new Date(), new Date(), sort);
-        } else if (state.equals("PAST")) {
-            bookings = bookingRepository.findByBookerIdAndEndIsBefore(userId, new Date(), sort);
-        } else if (state.equals("FUTURE")) {
-            bookings = bookingRepository.findByBookerIdAndStartIsAfter(userId, new Date(), sort);
-        } else {
-            try {
-                BookingStatus status = BookingStatus.valueOf(state);
-                bookings = bookingRepository.findByStatusAndBookerId(status, userId, sort);
-            } catch (IllegalArgumentException e) {
-                throw new ValidationException("Unknown state: " + state);
-            }
+        switch (state) {
+            case "ALL":
+                bookings = bookingRepository.findByBookerId(userId, sort);
+                break;
+            case "CURRENT":
+                bookings = bookingRepository.findByBookerIdAndEndIsAfterAndStartIsBefore(userId, new Date(), new Date(), sort);
+                break;
+            case "PAST":
+                bookings = bookingRepository.findByBookerIdAndEndIsBefore(userId, new Date(), sort);
+                break;
+            case "FUTURE":
+                bookings = bookingRepository.findByBookerIdAndStartIsAfter(userId, new Date(), sort);
+                break;
+            default:
+                try {
+                    BookingStatus status = BookingStatus.valueOf(state);
+                    bookings = bookingRepository.findByStatusAndBookerId(status, userId, sort);
+                } catch (IllegalArgumentException e) {
+                    throw new ValidationException("Unknown state: " + state);
+                }
+                break;
         }
         log.info(bookings.toString());
         return BookingMapper.bookingToBookingDto(bookings);
@@ -78,25 +83,31 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public List<BookingDto> findByStateAndOwner(String state, Long userId) {
-        List<Booking> bookings = new ArrayList<>();
+        List<Booking> bookings;
         User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
         Sort sort = Sort.by("end").descending();
         log.info(new Date().toString());
-        if (state.equals("ALL")) {
-            bookings = bookingRepository.findByItemOwnerId(userId, sort);
-        } else if (state.equals("CURRENT")) {
-            bookings = bookingRepository.findByItemOwnerIdAndEndIsAfterAndStartIsBefore(userId, new Date(), new Date(), sort);
-        } else if (state.equals("PAST")) {
-            bookings = bookingRepository.findByItemOwnerIdAndEndIsBefore(userId, new Date(), sort);
-        } else if (state.equals("FUTURE")) {
-            bookings = bookingRepository.findByItemOwnerIdAndStartIsAfter(userId, new Date(), sort);
-        } else {
-            try {
-                BookingStatus status = BookingStatus.valueOf(state);
-                bookings = bookingRepository.findByStatusAndItemOwnerId(status, userId, sort);
-            } catch (IllegalArgumentException e) {
-                throw new ValidationException("Unknown state: " + state);
-            }
+        switch (state) {
+            case "ALL":
+                bookings = bookingRepository.findByItemOwnerId(userId, sort);
+                break;
+            case "CURRENT":
+                bookings = bookingRepository.findByItemOwnerIdAndEndIsAfterAndStartIsBefore(userId, new Date(), new Date(), sort);
+                break;
+            case "PAST":
+                bookings = bookingRepository.findByItemOwnerIdAndEndIsBefore(userId, new Date(), sort);
+                break;
+            case "FUTURE":
+                bookings = bookingRepository.findByItemOwnerIdAndStartIsAfter(userId, new Date(), sort);
+                break;
+            default:
+                try {
+                    BookingStatus status = BookingStatus.valueOf(state);
+                    bookings = bookingRepository.findByStatusAndItemOwnerId(status, userId, sort);
+                } catch (IllegalArgumentException e) {
+                    throw new ValidationException("Unknown state: " + state);
+                }
+                break;
         }
         return BookingMapper.bookingToBookingDto(bookings);
     }
@@ -127,7 +138,7 @@ public class BookingServiceImpl implements BookingService {
         User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
         Booking booking = bookingRepository.findById(bookingId).orElseThrow(() -> new NotFoundException("Booking not found"));
         BookingStatus bookingStatus = approved ? BookingStatus.APPROVED : BookingStatus.REJECTED;
-        if (booking.getItem().getOwner().getId().equals(user.getId())) {
+        if (!booking.getItem().getOwner().getId().equals(user.getId())) {
             throw new NotFoundException("Only owner can change booking status");
         }
         if (!booking.getStatus().equals(BookingStatus.WAITING)) {
