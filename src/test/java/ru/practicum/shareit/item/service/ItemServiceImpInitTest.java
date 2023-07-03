28871a6d54a8242ecf9ded0fa250f4ef1d94ba1c
Illarionov Alex <linedaykek@gmail.com>
2023-07-03 13:08:51 +0300

    fix: ItemServiceImpInitTest correction

--- src/test/java/ru/practicum/shareit/item/service/ItemServiceImpInitTest.java
+++ src/test/java/ru/practicum/shareit/item/service/ItemServiceImpInitTest.java
@@ -10,6 +10,7 @@ import ru.practicum.shareit.booking.model.BookingStatus;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemsDto;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.CommentRepository;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.request.repository.RequestRepository;
@@ -31,9 +32,7 @@ class ItemServiceImpInitTest {
     private final ItemService itemService;
     private final ItemRepository itemRepository;
     private final UserRepository userRepository;
-    private final CommentRepository commentRepository;
     private final BookingRepository bookingRepository;
-    private final RequestRepository requestRepository;
 
     @Test
     void getItems() throws ParseException {
@@ -48,24 +47,24 @@ class ItemServiceImpInitTest {
         User user3 = new User(null, "User3", "User3@gmail.com");
         userRepository.save(user3);
 
-        ItemDto itemDto1 = new ItemDto(null, "item1", "description1", true, null, null);
-        ItemDto itemDto2 = new ItemDto(null, "item2", "description2", true, null, null);
-        itemService.create(itemDto1, user1.getId());
-        itemService.create(itemDto2, user1.getId());
+        Item item1  = new Item(null, "item1",user1, null ,"description1", true);
+        Item item2  = new Item(null, "item2",user1, null ,"description2", true);
+        itemRepository.save(item1);
+        itemRepository.save(item2);
 
-        Booking lastBooking = new Booking(null, formatter.parse("07-01-2013"), formatter.parse("07-02-2013"), itemRepository.findById(1L).get(), user2, BookingStatus.APPROVED);
-        Booking nextBooking = new Booking(null, formatter.parse("07-01-2050"), formatter.parse("07-01-2051"), itemRepository.findById(1L).get(), user3, BookingStatus.APPROVED);
+        Booking lastBooking = new Booking(null, formatter.parse("07-01-2013"), formatter.parse("07-02-2013"), item1, user2, BookingStatus.APPROVED);
+        Booking nextBooking = new Booking(null, formatter.parse("07-01-2050"), formatter.parse("07-01-2051"), item1, user3, BookingStatus.APPROVED);
 
         bookingRepository.save(lastBooking);
         bookingRepository.save(nextBooking);
 
         List<ItemsDto> itemsDtos = itemService.getItems(user1.getId(), 0, 20);
         assertThat(itemsDtos.size(), equalTo(2));
-        assertThat(itemsDtos.get(0).getName(), equalTo(itemDto1.getName()));
-        assertThat(itemsDtos.get(0).getDescription(), equalTo(itemDto1.getDescription()));
+        assertThat(itemsDtos.get(0).getName(), equalTo(item1.getName()));
+        assertThat(itemsDtos.get(0).getDescription(), equalTo(item1.getDescription()));
         assertThat(itemsDtos.get(0).getLastBooking().getBookerId(), equalTo(user2.getId()));
         assertThat(itemsDtos.get(0).getNextBooking().getBookerId(), equalTo(user3.getId()));
-        assertThat(itemsDtos.get(1).getName(), equalTo(itemDto2.getName()));
-        assertThat(itemsDtos.get(1).getDescription(), equalTo(itemDto2.getDescription()));
+        assertThat(itemsDtos.get(1).getName(), equalTo(item2.getName()));
+        assertThat(itemsDtos.get(1).getDescription(), equalTo(item2.getDescription()));
     }
 }
